2a6ea223c91bb6d8af389f6fd8f785f0e4c8c293
maartenschroons <r0761272@student.thomasmore.be>
2020-01-27 08:44:09 +0100

    H

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { MatFormFieldModule, MatInputModule, MatButtonModule, MatSidenavModule, 
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [],
